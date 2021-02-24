d4606578bed8798a760a7da507dd70ea59c2416d
Fabian Rivera <frakxman@hotmail.com>
2021-02-23 19:08:36 -0500

    Initial modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Components
+import { AddComponent } from './pages/add/add.component';
+import { HeroComponent } from './pages/hero/hero.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
+import { SearchComponent } from './pages/search/search.component';
+
+
+@NgModule({
+  declarations: [
+    AddComponent,
+    SearchComponent,
+    HeroComponent,
+    HomeComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeroesModule { }
