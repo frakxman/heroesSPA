a1d1e86e9cf42b16678af6d7952ef10778cc601b
Fabian Rivera <frakxman@hotmail.com>
2021-02-24 09:53:49 -0500

    List component css styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 // Components
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
@@ -14,9 +15,10 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
     ErrorPageComponent
   ],
   imports: [
-    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/pages/list/list.component.ts
+++ src/app/heroes/pages/list/list.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styles: [
-  ]
+  styles: [`
+    mat-card {
+      margin-top: 10px;
+    }
+  `]
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[] = [];
+
+  constructor( private heroesService: HeroesService ) { }
 
   ngOnInit(): void {
+
+    this.heroesService.getHeroes()
+      .subscribe( heroes => this.heroes = heroes );
   }
 
 }
